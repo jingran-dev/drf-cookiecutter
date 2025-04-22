54aba923fc8f8bf03c56fa2a7c95cf3aab4023e8
zhoujingryan <jingranzhou@126.com>
2025-04-22 14:12:43 +0800

    add black to precommit

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -30,7 +30,7 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 
 # Internal IPs for debug toolbar
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+INTERNAL_IPS = ["127.0.0.1"]
 
 # SILK SETTINGS
 # ------------------------------------------------------------------------------
